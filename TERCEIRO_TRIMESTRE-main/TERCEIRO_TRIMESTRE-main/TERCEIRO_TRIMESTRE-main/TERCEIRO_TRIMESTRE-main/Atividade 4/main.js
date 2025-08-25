50617789e52401484e456309ace51ec787cde826
leandro0405 <162132097+leandro0405@users.noreply.github.com>
2025-08-25 11:55:58 -0300

    Add files via upload

---
+++ TERCEIRO_TRIMESTRE-main/TERCEIRO_TRIMESTRE-main/TERCEIRO_TRIMESTRE-main/TERCEIRO_TRIMESTRE-main/Atividade 4/main.js
@@ -0,0 +1,14 @@
+// cria referẽncia ao form e ao elemento h3 (onde será exibida a resposta)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+// cria um "ouvinte" de evento, acionado quando o botão submi for clicado
+frm.addEventListener("submit", (e) => {
+    const quilo = Number(frm.InQuilo.value); //obtém conteúdo dos campos
+    const consumo = Number(rm.inConsumo.value);
+
+    const valor = (quilo / 1000) * consumo; // calcula valor a ser pago
+    resp.innerText = `Valor a pagar: R$ ${valor.toFixed(2)}`; // exibe resposta
+
+  e.preventDefault();           // evita envio do form
+});
